7e07c82d40f424bb36c666c8c4489b7a97bb8156
Kyrylo Pustovit <90320215+KyryloPustovit@users.noreply.github.com>
2021-11-30 20:13:20 +0200

    Add files via upload

--- Lab #5/1.py
+++ Lab #5/1.py
@@ -2,10 +2,11 @@
 
 #ППараметри книги
 class Book:
-    def __init__(Slf, author, Date, genre):
+    def __init__(Slf, author, Rik, genre,Title):
         Slf.author=author
-        Slf.Date=Date
+        Slf.Rik=Rik
         Slf.genre=genre
+        Slf.Title=Title
 
 
 #Бібліотека
@@ -33,10 +34,10 @@ class Library:
     
     
     #Пошук по даті
-    def searchByDate(Slf, Date):
+    def searchByRik(Slf, Rik):
         books = []
         for book in Slf.books:
-            if book.Date == Date:
+            if book.Rik == Rik:
                 books.append(book)
         return books
     
@@ -51,29 +52,23 @@ class Library:
 
 
 #Книги
-def main():
-    library = Library()
+library = Library()
 
-    library.addBook(Book('Іва́н Нечу́й-Леви́цький', 'Кайдашева сімя', 1878, 'повість'))
-    library.addBook(Book('Артур Чарльз Кларк', '2001: Космічна Одіссея', 1968, 'Научная фантастика'))
-    library.addBook(Book('Михайло Коцюбинський', 'Тіні забутих предків', 1912, 'драма'))
-    library.addBook(Book('Артур Чарльз Кларк', '2010: Одіссея Два', 1982, 'Научная фантастика'))
-    library.addBook(Book('Артур Чарльз Кларк', 'Око Часу', 2003, 'Научная Фантастика'))
+library.addBook(Book('Іва́н Нечу́й-Леви́цький', 'Кайдашева сімя', 1878, 'повість'))
+library.addBook(Book('Артур Чарльз Кларк', '2001: Космічна Одіссея', 1968, 'Научная фантастика'))
+library.addBook(Book('Михайло Коцюбинський', 'Тіні забутих предків', 1912, 'драма'))
+library.addBook(Book('Артур Чарльз Кларк', '2010: Одіссея Два', 1982, 'Научная фантастика'))
+library.addBook(Book('Артур Чарльз Кларк', 'Око Часу', 2003, 'Научная Фантастика'))
 
     
     #Сортування по автору
-    author = 'Артур Чарльз Кларк'
-    books = library.searchByAuthor(author)
-    if len(books) == 0:
-        return
+author = 'Артур Чарльз Кларк'
+books = library.searchByAuthor(author)
+if len(books) != 0:
     book = books[0]
     print('Кількість книг автора ' + author, len(books))
     print('Автор книги', author)
+    print('Титульна сторынка книги', book.Title)
     library.removeBook(book)
     books = library.searchByAuthor(author)
     print('Нова кількість книг автора ' + author, len(books))
-
-
-#Кінець
-if __name__ == '__main__':
-    main()
