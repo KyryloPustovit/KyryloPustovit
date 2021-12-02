e2ec98a05ae6171472910d55433e2133f7ee9f96
Kyrylo Pustovit <90320215+KyryloPustovit@users.noreply.github.com>
2021-12-02 20:09:10 +0200

    Update 1.py

--- Lab#7/1.py
+++ Lab#7/1.py
@@ -1,8 +1,8 @@
 from numpy import *
 import matplotlib.pyplot as plt
 
-x = linspace[-4,4] 
-y = Y(x)=5*sin(1/x)*cos(x^2)^3
+x=linspace(-4,4,51) 
+y=5*sin(1/x)*cos(x**2)**3
 
 plt.plot(x, y)
 plt.show()
